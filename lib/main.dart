98b225a011761d92683cb940fec6e15cca025edc
myid-hana <58727114+myid-hana@users.noreply.github.com>
2020-05-14 17:20:04 +0900

    ListTile에 입력값 받기
    ListTile을 따로 위젯처리하고 입력값을 변수처리함.
    map, toList를 통해 ListView에 깔아줌.

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,13 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
+class Todo{
+  bool isDone = false;  //처음에는 false로 선언해준다.
+  String title; //입력값을 저장할 문자열 변수
+
+  Todo(this.title);  //입력값을 title에 저장
+}
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -21,14 +28,35 @@ class TodoApp extends StatefulWidget {
 }
 
 class _TodoAppState extends State<TodoApp> {
+
+  final _item = <Todo>[];  //Todo 값을 받아서 비어있는 배열을 만들어준다.
+
+  Widget _buildItemWidget(Todo todo){   //Todo의 isDone과 title을 todo로 받는다.
+    return ListTile(
+      onTap: () {   //ListTile을 누를 때마다
+        setState(() {   //상태가 변한다.
+          todo.isDone = !todo.isDone;   //true - false 로 변환됨.
+        });
+      },
+      title: Text(
+        todo.title,
+        style: todo.isDone ? TextStyle(    //isDone이 true이면 style적용, false이면 style : null
+          decoration: TextDecoration.lineThrough,
+          fontStyle: FontStyle.italic,
+        ) : null,
+      ),
+      trailing: IconButton(
+        icon: Icon(Icons.delete_forever),
+      ),
+    );
+  }
+
   void _toggleTodo(todo) {
     setState(() {
       //상태변화
       todo.isDone = !todo.isDone;
     });
   }
-
-  bool isDone = false;  //처음에는 false로 선언해준다.
 
   @override
   Widget build(BuildContext context) {
@@ -56,25 +84,7 @@ class _TodoAppState extends State<TodoApp> {
             ),
             Expanded(
               child: ListView(
-                children: <Widget>[
-                  ListTile(
-                    onTap: () {   //ListTile을 누를 때마다
-                      setState(() {   //상태가 변한다.
-                        isDone = !isDone;   //true - false
-                      });
-                    },
-                    title: Text(
-                      "to do 1",
-                      style: isDone ? TextStyle(    //isDone이 true이면 style적용, false이면 style : null
-                        decoration: TextDecoration.lineThrough,
-                        fontStyle: FontStyle.italic,
-                      ) : null,
-                    ),
-                    trailing: IconButton(
-                      icon: Icon(Icons.delete_forever),
-                    ),
-                  ),
-                ],
+                children: _item.map((todo) => _buildItemWidget(todo)).toList(),
               ),
             ),
           ],
